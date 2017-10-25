65a7a30358d5e1ff7e9bc71bde129dc237783cb4
Robert Sasak <robert.sasak@gmail.com>
2017-10-25 16:14:45 +0200

    Smart app banner

--- main.js
+++ main.js
@@ -9,6 +9,28 @@ if ('serviceWorker' in navigator) {
         });
 }
 
+new SmartBanner({
+    daysHidden: 15,   // days to hide banner after close button is clicked (defaults to 15)
+    daysReminder: 20, // days to hide banner after "VIEW" button is clicked (defaults to 90)
+    appStoreLanguage: 'no', // language code for the App Store (defaults to user's browser language)
+    title: 'Norgeskart',
+    author: 'Norgeskart',
+    button: 'VIEW',
+    store: {
+        ios: 'On the App Store',
+        android: 'In Google Play',
+        windows: 'In Windows store'
+    },
+    price: {
+        ios: 'FREE',
+        android: 'FREE',
+        windows: 'FREE'
+    }
+    // , theme: '' // put platform type ('ios', 'android', etc.) here to force single theme on all device
+    // , icon: '' // full path to icon image if not using website icon image
+    // , force: 'ios' // Uncomment for platform emulation
+});
+
 var layers = {
     land: L.tileLayer('https://opencache{s}.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
         // bounds: [
